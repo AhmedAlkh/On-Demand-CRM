1c76a6b00c6bf313538a3fec82f0794ef5d53ce5
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-01-27 16:03:47 -0500

    created connection query select all from user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,28 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 // Connection Pool
 const pool = mysql.createPool({
-    connectionLimit: 100,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
-
-// Connect to DB
-pool.getConnection((err, connection) => {
-    if(err) throw err; // not connected!
-    console.log('Connected as ID ' + connection.threadId);
+  connectionLimit: 100,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 // View users
 exports.view = (req, res) => {
-    res.render('home');
-}
+  pool.getConnection((err, connection) => {
+    if (err) throw err; // not connected!
+    console.log("Connected as ID " + connection.threadId);
+
+    // User connection
+    connection.query('SELECT * FROM user', (err, rows) => {
+        connection.release();
+        if (!err) {
+            res.render('home');
+        } else {
+            console.log(err);
+        }
+    });
+  });
+};
