76f9a72034e9d838cb4ec7a8c6549d256e8d89e5
el bandingo <elbandingo@protonmail.com>
2022-02-06 19:17:11 -0330

    testing delete refresh

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,11 +2,7 @@ const mysql = require("mysql2");
 
 // Connection Pool
 const pool = mysql.createPool({
-  connectionLimit: 100,
-  host: "l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "o3jbsof69fg94bs8",
-  password: "gqw33e8mhofjyexp",
-  database: "wzn3hb0ra6xy1my8"
+uri: process.env.JAWSDB_URL
 })
 
 // View users
@@ -148,7 +144,10 @@ exports.delete = (req,res) => {
       connection.query('DELETE FROM customers WHERE id = ?', [req.params.id], (err, rows) => {
         connection.release();
         if (!err) {
-            res.redirect('/');
+          connection.query('SELECT * FROM customers', (err, rows) => {
+            res.render('index', {rows});
+          });
+            
         } else {
             console.log(err);
         }
