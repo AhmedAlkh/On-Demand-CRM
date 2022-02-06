b9215662052619e3170b4627822ecc2f62f86e5d
el bandingo <elbandingo@protonmail.com>
2022-02-06 18:58:55 -0330

    testing uri deploy

--- server.js
+++ server.js
@@ -33,13 +33,8 @@ app.set('view engine', 'hbs');
 
 //create a connection pool
 const pool = mysql.createPool({
-      connectionLimit: 100,
-      host: "l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-      user: "o3jbsof69fg94bs8",
-      password: "gqw33e8mhofjyexp",
-      database: "wzn3hb0ra6xy1my8"
-})
-
+      uri: process.env.JAWSDB_URL
+      })
 //connect to the database
 pool.getConnection((error, connection) => {
       if(error) throw error;
