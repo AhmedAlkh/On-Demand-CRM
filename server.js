2320b5f5f417b05829d7eae97350fee4e2e9d9e1
el bandingo <elbandingo@protonmail.com>
2022-02-06 18:39:56 -0330

    testing a push to heroku

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.set('view engine', 'hbs');
 const pool = mysql.createPool({
       connectionLimit: 100,
       host: "l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-      user: o3jbsof69fg94bs8,
-      password: gqw33e8mhofjyexp,
-      database: wzn3hb0ra6xy1my8
+      user: "o3jbsof69fg94bs8",
+      password: "gqw33e8mhofjyexp",
+      database: "wzn3hb0ra6xy1my8"
 })
 
 //connect to the database
